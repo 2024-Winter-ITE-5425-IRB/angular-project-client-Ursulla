db0c3438cf31e3f3cd637af4db03ef8cae9aa997
Arivola Ursulla <ursullaarivola@gmail.com>
2024-04-15 20:10:24 -0400

    Ursulla commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { AddRoomComponent } from './components/add-room/add-room.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegisterComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent, HeaderComponent, FooterComponent, RoomsComponent, AddRoomComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { AddRoomComponent } from './components/add-room/add-room.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
+    { path: 'rooms', component: RoomsComponent },
+    { path: 'add-room', component: AddRoomComponent },
+    { path: 'about', component: AboutComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
    //{ path: '**', redirectTo: '/login', pathMatch: 'full' }
